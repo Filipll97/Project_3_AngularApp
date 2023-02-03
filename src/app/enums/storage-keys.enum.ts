df1b840e3ea0286bb7a0b1f1ffbd5001a4dfe0f4
Filipll97 <filiplindell2@gmail.com>
2023-02-03 11:37:19 +0100

    feat: added a pokemon catalogue with pictures and names

--- src/app/enums/storage-keys.enum.ts
+++ src/app/enums/storage-keys.enum.ts
@@ -1,3 +1,4 @@
 export enum StorageKeys {
-    Trainer = "pokemon-trainer"
+    Trainer = "pokemon-trainer",
+    PokemonUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
 }
