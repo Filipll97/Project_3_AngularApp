fd6e6a8ef7affdc470f9a0519ce9f8e0278fb5d9
Filipll97 <filiplindell2@gmail.com>
2023-02-06 14:22:35 +0100

    fix: Fixed link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiTrainers: "https://cypress-subdued-receipt.glitch.me/trainers",
-  apiPokemons: "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=999%22",
+  apiTrainers: "http://cypress-subdued-receipt.glitch.me/trainers",
+  apiPokemons: "http://pokeapi.co/api/v2/pokemon/?offset=0&limit=999%22",
   apiKey: "q=<M(qpHgSX}_-@7BGq>f,QVU~:pCZ_=eV7N][-Rkw!#JJErJxwu^b!e96:(2E=",
 }
