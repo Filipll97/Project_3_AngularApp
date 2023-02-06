3fa1d10cb5cdca75eb08b5fc9a4edb10ecf47b0b
Filipll97 <filiplindell2@gmail.com>
2023-02-06 14:09:31 +0100

    fix: Fixed the environment folder

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiTrainers: "http://cypress-subdued-receipt.glitch.me/trainers",
+  apiPokemons: "http://pokeapi.co/api/v2/pokemon/?offset=0&limit=999%22",
+  apiKey: "q=<M(qpHgSX}_-@7BGq>f,QVU~:pCZ_=eV7N][-Rkw!#JJErJxwu^b!e96:(2E=",
+}
